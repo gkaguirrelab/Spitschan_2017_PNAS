1e2ebe1dd2d29c3ee0d539a8350f4644b4c8854e
Manuel Spitschan <spitschan@stanford.edu>
2017-05-18 09:12:44 -0700

    Updates
    Signed-off-by: Manuel Spitschan <spitschan@stanford.edu>

--- code/uberScript.m
+++ code/uberScript.m
@@ -1,56 +1,60 @@
-
+%% Master script for Spitschan et al. (2017)
 %
-% Overall master script for Spitschan et al. 2017 Nature Neuroscience
+% Reference: Spitschan M, Bock A, Ryan J, Frazzetta G, Brainard DH, Aguirre
+% GK (2017) The human visual cortex response to melanopsin-directed
+% stimulation is accompanied by a distinct perceptual experience, bioRxiv,
+% https://doi.org/10.1101/138768
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% (0) Dependencies
+% This script requires the following repositories to be local on your
+% machine:
+%           https://github.com/gkaguirrelab/fmriMelanopsinMRIAnalysis/
+%           https://github.com/gkaguirrelab/psychoMelanopsinAnalysis/
+%           https://github.com/gkaguirrelab/pupilMelanopsinMRIAnalysis/
+%           https://github.com/spitschan/splatterMelanopsinMRIAnalysis
 %
+% It relies heavily on automatic path configuration using ToolboxToolbox:
+%           https://github.com/ToolboxHub/ToolboxToolbox
 
-
-%%  PART 1. MRI data processing
-
-% reference repo: 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% (1) MRI data processing
+% Reference repository:
 %           https://github.com/gkaguirrelab/fmriMelanopsinMRIAnalysis
-% Additional toolboxes deployed by ToolboxToolbox: 
-%           MRklar (MaxMel_paper branch) 
-%           MRlyze (MaxMel_paper branch) 
-%           temporalFittingEngine
 
-
-%% Before starting: make sure all files are in place
-% 1.Decompress the full dataset in your "dataDir". Do not alter the path
+%% (1a) Before starting: make sure all files are in place
+% 1. Uncompress the full dataset in your "dataDir". Do not alter the path
 % structure of dataDir.
-
-% 2. Decompress the freesurferSubjects dir and copy the content in your
+% 2. Uncompress the freesurferSubjects dir and copy the content in your
 % freesurfer subject folder.
+% 3. Uncompress the anatTemplate archive in your anatTemplateDir.
 
-% 3. decompress the anatTemplate archive in your anatTemplatedDir
-
-%% Set toolboxtoolbox configuration for fMRI analysis
-
+%% (1b) Set up ToolboxToolbox configuration for fMRI analysis
 tbUseProject('fmriMelanopsinMRIAnalysis');
 
-%% Define initial paths and Parameters
-
-params.resultsDir =  '/data/jag/MELA/MelanopsinMR/results';
+%% (1c) Define initial paths and Parameters.
+% <!> NOTE: This need to be adjusted for your local structure.
+params.resultsDir = '/data/jag/MELA/MelanopsinMR/results';
 params.logDir = '/data/jag/MELA/MelanopsinMR/logs';
 params.dataDir = '/data/jag/MELA/MelanopsinMR';
 params.anatTemplateDir = '/data/jag/MELA/anat_templates';
 
-%% Create preprocessing scripts
-% <!> This cell assumes that the freesurfer subjects files have been copied to
-% the local freesurfer subject folder. This will save time while doing
+%% (1d) Create preprocessing scripts
+% <!> NOTE: This cell assumes that the freesurfer subjects files have been copied
+% to the local freesurfer subject folder. This will save time while doing
 % preprocessing, as the recon all step will be skipped.
-
 fmriMelanopsinMRIAnalysis_createPreprocessingScripts(params);
 
-%% Run the preprocessing scripts
-% <!> Evaluating this cell will submit the scripts one by one, one after
+%% (1e) Run the preprocessing scripts
+% <!> NOTE: Evaluating this cell will submit the scripts one by one, one after
 % the other. This will likely take serveral days to complete. If you have
-% access to a unix cluster architecture, consider navigating in each session
-% directoru and launching the job script called "submit_<subjectName>_all.sh"
-
-
-%%%%%%%%%%  NOTE to GIULIA: this works only on MAC and Linux
-
-
+% access to a unix cluster architecture, consider navigating in each
+% session directoru and launching the job script called
+% "submit_<subjectName>_all.sh"
+%
+% <!> NOTE: At present this only words on OS X and Linux.
 system (sprintf (['sh ' params.dataDir '/HERO_asb1/032416/preprocessing_scripts/HERO_asb1_MaxMel_all.sh']))
 system (sprintf (['sh ' params.dataDir '/HERO_asb1/040716/preprocessing_scripts/HERO_asb1_MaxMel_all.sh']))
 system (sprintf (['sh ' params.dataDir '/HERO_asb1/051016/preprocessing_scripts/HERO_asb1_MaxMel_all.sh']))
@@ -79,44 +83,68 @@ system (sprintf (['sh ' params.dataDir '/HERO_mxs1/060916/preprocessing_scripts/
 system (sprintf (['sh ' params.dataDir '/HERO_mxs1/061016_Mel/preprocessing_scripts/HERO_mxs1_MaxMel_all.sh']))
 system (sprintf (['sh ' params.dataDir '/HERO_mxs1/101916/preprocessing_scripts/HERO_mxs1_MaxMel_all.sh']))
 system (sprintf (['sh ' params.dataDir '/HERO_mxs1/102416/preprocessing_scripts/HERO_mxs1_MaxMel_all.sh']))
-    
-%% Make anatomical templates
+
+%% (1f) Make anatomical templates
 fmriMelanopsinMRIAnalysis_makeAnatTemplates(params);
 
-%% Project anatomical templates
+%% (1g) Project anatomical templates
 fmriMelanopsinMRIAnalysis_projectAnatTemplatesToFunc(params);
 
 
-%% Assemble V1 time series for the CRF data
+%% (1h) Assemble V1 time series for the CRF data
 fmriMelanopsinMRIAnalysis_makeAllCRFPackets(params);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% (2) Whole-brain chi-square maps
+
+% <!> Fill in
 
 
-%%  PART 2. Full brain ChiSquare Maps
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% (3) Pupillometry data analysis
+% Reference repository:
+%           https://github.com/gkaguirrelab/pupilMelanopsinMRIAnalysis
+
+%% (3a) Before starting: make sure all files are in place
+
+%% (3b) Set up ToolboxToolbox configuration for pupil analysis
+tbUseProject('pupilMelanopsinMRIAnalysis');
+
+%% (3c) Pupil data analysis
+pupilPMEL_main();
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% (4) Psychophysical data analysis
+% Reference repository:
+%           https://github.com/gkaguirrelab/pupilMelanopsinMRIAnalysis
+
+%% (4a) Before starting: make sure all files are in place
+
+%% (4b) Set up ToolboxToolbox configuration for psychophysical data analysis
+tbUseProject('psychoMelanopsinMRIAnalysis');
+
+%% (4c) Pupil data analysis
+psychoMelAnalysis_main();
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% (5) Physiological splatter analysis
+% Reference repository:
+%           https://github.com/gkaguirrelab/splatterMelanopsinMRIAnalysis
+
+%% (5a) Before starting: make sure all files are in place
 
 
+%% (5b) Set up ToolboxToolbox configuration for splatter analysis
+tbUseProject('splatterMelanopsinMRIAnalysis');
 
-% code here
-
-
-
-%%  PART 3. Pupil data Analysis
-
-
-
-% code here
-
-
-
-
-
-%%  PART 4. Figure making
-
-
-
-% code here
-
-
-
-
-
+%% (5c) Splatter data analysis
+splatterMel_AttentionTask();
+splatterMel_PerceptualDataSplatter();
+splatterMel_PhysiologicalSplatterAnalysis();
+splatterMel_SpectraTable();
+splatterMel_SpectralPlots();
+splatterMel_SplatterAnalysis();
